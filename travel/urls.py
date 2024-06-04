dc073946512457e4ef78e6c56dc506c2d2306f51
TsengYuFei <sophie99250@gmail.com>
2024-06-04 21:48:48 +0800

    修改圖片路徑，會員頭像顯示正常，會員主頁(detail.html)大致完成

--- travel/urls.py
+++ travel/urls.py
@@ -5,7 +5,11 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('edit_article/', views.edit_article, name='edit_article'),
     path('post_detail/', views.post_detail, name='post_detail'),
-
     path('add_membership/', views.add_member, name = 'add_member'),
     path('<str:member_id>/detail/', views.detail, name = 'detail'),
 ]
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
